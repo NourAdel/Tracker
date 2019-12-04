5e9a43d65f7a9c9cf8daae535b804c5dad34f4c1
Nour Adel <noura7305@gmail.com>
2019-12-04 15:50:46 +0200

    connecting mongo instance

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0-ab55u.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to monge instance");
+});
+
+mongoose.connection.on("error", err => {
+  console.log("Error connecting to mongo: ", err);
+});
 
 app.get("/", (req, res) => {
   res.send("hi there!");
