a5b533206a89e466fdabae93fe65847e97065876
Nour Adel <noura7305@gmail.com>
2019-12-05 12:38:40 +0200

    Sign-up styling

--- Tracker/src/screens/SignupScreen.js
+++ Tracker/src/screens/SignupScreen.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import IconE from 'react-native-vector-icons/AntDesign';
+import Iconp from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Button, Text, Input} from 'react-native-elements';
+
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({navigation}) => {
   return (
-    <>
-      <Text style={{fontSize: 48}}>SignupScreen</Text>
-      <Button
-        title="Sign In"
-        onPress={() => navigation.navigate('Signin')}></Button>
-        <Button
-        title="main flow"
-        onPress={() => navigation.navigate('mainFlow')}></Button>
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>TRACKER</Text>
+      </Spacer>
+      <Input
+        placeholder="Enter you Emial"
+        leftIcon={<IconE name="mail" size={24} color={'black'} />}
+      />
+      <Spacer />
+      <Input
+        placeholder="Enter you password"
+        leftIcon={<Iconp name="onepassword" size={24} color={'black'} />}
+      />
+      <Spacer>
+        <Button title="Sign Up" type="outline" />
+      </Spacer>
+      </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOption = () => {
+  return {header: null};
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent:'center',
+    marginBottom:100
+  }
+});
 
 export default SignupScreen;
