0a4f2666ca49cc43b3fb153f2ab2e228736ed6f7
Nour Adel <noura7305@gmail.com>
2019-12-05 13:14:10 +0200

    setting up context

--- Tracker/src/screens/SignupScreen.js
+++ Tracker/src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import IconE from 'react-native-vector-icons/AntDesign';
 import Iconp from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -7,6 +7,9 @@ import {Button, Text, Input} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPasword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
@@ -14,30 +17,41 @@ const SignupScreen = ({navigation}) => {
       </Spacer>
       <Input
         placeholder="Enter you Emial"
+        value={email}
+        onChangeText={text => setEmail(text)}
         leftIcon={<IconE name="mail" size={24} color={'black'} />}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Spacer />
       <Input
         placeholder="Enter you password"
+        value={password}
+        onChangeText={text => setPasword(text)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
         leftIcon={<Iconp name="onepassword" size={24} color={'black'} />}
       />
       <Spacer>
         <Button title="Sign Up" type="outline" />
       </Spacer>
-      </View>
+    </View>
   );
 };
 
-SignupScreen.navigationOption = () => {
-  return {header: null};
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null,
+  };
 };
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    justifyContent:'center',
-    marginBottom:100
-  }
+    justifyContent: 'center',
+    marginBottom: 150,
+  },
 });
 
 export default SignupScreen;
